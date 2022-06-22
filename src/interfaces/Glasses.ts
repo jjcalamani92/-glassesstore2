e9625f82680b6aa9a10569230790408f5b4826f0
Jesus Calamani <jesuscalamani92@gmail.com>
2022-06-22 09:40:04 -0400

    v

---
+++ src/interfaces/Glasses.ts
@@ -0,0 +1,24 @@
+export interface IGlasses {
+	_id: string;
+	name: string;
+	brand: string;
+	image: string[];
+	description: string;
+	inStock: number;
+	slug: string;
+	category: string;
+	section: string;
+	item: string;
+	price: number;
+	oldPrice: number;
+	tags: string[];
+	client: string;
+	status: boolean;
+	site: string;
+
+	glasses: string;
+	form: string;
+	bridge: string;
+	rod: string;
+
+}
